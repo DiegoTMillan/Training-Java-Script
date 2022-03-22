f55c6d62a34be1edb9e743c0f56f924a7099d266
Diego <diego_tapia_millan@yahoo.es>
2022-03-22 20:32:35 +0100

    despues de al correccion del profe

--- training-javascript/js/app.js
+++ training-javascript/js/app.js
@@ -102,7 +102,7 @@
 //     console.log("out of range");
 // }
 
-//Exercise 1 
+//Exercise 1
 
 // let today = new Date();
 // let actMonth = today.getMonth();
@@ -113,7 +113,7 @@
 //   month = "Marzo";
 // } else {
 //   month = "cualquier mes menos marzo"
-// } 
+// }
 // console.log(month)
 
 //Exercise 2
@@ -147,14 +147,16 @@
 //   alert("That's not a real month!");
 // }
 
-//Exercise 3
+//Exercise 3 (en este ejercicio se podría quitar del segundo case el >=15 porque ya en el primero
+//no está incluido con lo que se puede obviar, pues será menor de 25 o será error y nos ahorramos una línea)
 
 // let extTemperature = prompt ("What temperature is it?");
+// console.log(typeof extTemperature)
 // switch (true) {
 //   case extTemperature < 15:
 //     alert("Warm up");
 //     break;
-//   case extTemperature > 15 && extTemperature < 25:
+//   case extTemperature >= 15 && extTemperature <= 25:
 //     alert("Enjoy the weather!");
 //     break;
 //   case extTemperature > 25:
@@ -176,66 +178,217 @@
 //     break;
 // }
 
-//Exercise 5 ESTÁ MAL, NO FUNCIONA
-// let absoluteZero = 273.15;
-// let freezingPoint = 0;
-// let bodyTemperature = 37;
-// let boillingPoint = 100; 
-// let celsius = absoluteZero || freezingPoint || boillingPoint || bodyTemperature;
-// let resultado = celsius * 9/5 + 32;
+//Option 4.2 mejor que el primero, es incorrecto poner el booleano en el if.
 
-// switch (resultado){
-//   case absoluteZero == (273.15 *1.8 + 32):
-//     console.log(fahrenhait);
+// let isGreen = true;
+// if (isGreen){
+//   console.log("Cross the road");
+// }else{
+//   console.log("Please await");
+// }
+
+//Exercise 5
+//.toFixed(2) redondea a 2 cifras
+
+// let temperature = "bodyTemperature";
+// let conversion = "";
+
+// switch (temperature){
+//   case "absoluteZero":
+//     conversion = ((-273.15 * 1.8 + 32).toFixed(2));
+//     console.log(`absolute zero degrees are ${conversion} ºF`);
 //     break;
-//   case freezingPoint = (0 * 1.8 + 32):
-//     console.log(freezingPoint , "ºF");
+//   case "freezingPoint":
+//     conversion = ((0 * 1.8 + 32).toFixed(2));
+//     console.log(`freezing point degrees are ${conversion} ºF`);
 //     break;
-//   case bodyTemperature = (37 * 1.8 + 32):
-//     console.log(bodyTemperature , "ºF");
+//   case "bodyTemperature":
+//     conversion = ((37 * 1.8 + 32).toFixed(2));
+//     console.log(`the body temperature is ${conversion} ºF`);
 //     break;
-//   case boillingPoint = (100 * 1.8 + 32):
-//     console.log(boillingPoint , "ºF")
+//   case "boillingPoint":
+//     conversion = ((100 * 1.8 +32).toFixed(2));
+//     console.log(`boilling point degrees are ${conversion} ºF`)
+//   default:
+//     console.log(unknown)
 // }
 
 //Exercise 6
+
 // let email = "tolkien@lordofrings.com";
 // let password = "aNBR6ZeWPY";
-// let emailCheck = prompt("Please, insert your email");
-// let passwordCheck = prompt ("Please insert your password");
-// if (emailCheck == email && passwordCheck == password) {
+// let userEmail = prompt("Please, insert your email");
+// let userPassword = prompt ("Please insert your password");
+// if (userEmail == email && userPassword == password) {
 //   alert("Welcome Mr. Tolkien");
-// }else if (emailCheck !== email || passwordCheck !== password){
+// }else if (userEmail !== email || userPassword !== password){
 //   alert("Wrong password or username");
 // }
 
 //Exercise 7
-let currency = "yen";
-let euro = 200;
-let change = "";
+//Si quitamos los break, saldrán todos los resultados uno debajo del otro si se pone
+//la primera variable claro.
 
-switch (currency) {
-  case "dolar":
-    change = euro * 1.10;
-    console.log(`${euro} euros son ${change} dólares al cambio`);
-    break; 
-  case "yen":
-    change = euro * 132.87;
-    console.log(`${euro} euros son ${change} yenes al cambio`);
-    break;
-  case "libra":
-    change = euro * 0.83;
-    console.log(`${euro} euros son ${change} libras esterlinas al cambio`);
-    break;
-  case "franco":
-    change = euro * 1.03;
-    console.log(`${euro} euros son ${change} francos suizos al cambio`);
-    break;
-  default:
-    console.log(unknown);   
+// let currency = "dolar";
+// let euro = 200;
+// let change = "";
+
+// switch (currency) {
+//   case "dolar":
+//     change = (euro * 1.10).toFixed(2);
+//     console.log(`${euro} euros son ${change} dólares al cambio`);
+//     // break;
+//   case "yen":
+//     change = (euro * 132.87).toFixed(2);
+//     console.log(`${euro} euros son ${change} yenes al cambio`);
+//     // break;
+//   case "libra":
+//     change = (euro * 0.83).toFixed(2);
+//     console.log(`${euro} euros son ${change} libras esterlinas al cambio`);
+//     // break;
+//   case "franco":
+//     change = (euro * 1.03).toFixed(2);
+//     console.log(`${euro} euros son ${change} francos suizos al cambio`);
+//     // break;
+//   default:
+//     console.log(unknown);
+// }
+
+//Exercise 8
+
+// let number1  = prompt ("Please write one number");
+// let number2 = prompt ("Please, write another one");
+// let operation = prompt ("Please, choose between: +, -, x or /");
+// let result = "";
+// let add = "+";
+// let substract = "-";
+// let multiply = "*";
+// let divide = "/";
+
+// if (operation == add){
+//   result = parseInt(number1) + parseInt(number2);
+//   console.log(result)
+// }else if (operation == substract){
+//   result = number1 - number2;
+//   console.log(result);
+// }else if (operation == multiply){
+//   result = (number1 * number2);
+//   console.log(result);
+// }else if (operation == divide){
+//   result = (number1 / number2);
+//   console.log(result);
+// }
+
+//Exercise 8 , pero bien hecho
+
+// let num1 = prompt("Please, enter the first number");
+// let num2 = prompt("Please, enter the second number");
+// let operation = prompt("Please, choose one option: add, substract, multiply or divided");
+
+// switch (operation) {
+//   case "add":
+//     console.log(parseFloat(num1) + parseFloat(num2));
+//     break;
+//   case "substract":
+//     console.log(parseFloat(num1) - parseFloat(num2));
+//     break;
+//   case "multiply":
+//     console.log(parseFloat(num1) * parseFloat(num2));
+//     break;
+//   case "divie":
+//     console.log(parseFloat(num1) / parseFloat(num2));
+//     break;
+//   default:
+//     console.log("please choose a valid option.");
+// }
+
+//Exercise Opt "califications"
+
+// let califications = prompt("Please, enter your calification");
+// switch (parseInt(califications)) {
+//   case 0:
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     alert("failure")
+//     break;
+//   case 5:
+//     alert("passed");
+//     break;
+//   case 6:
+//     alert("well");
+//     break;
+//   case 7:
+//   case 8:
+//     alert("remarkable");
+//     break;
+//   case 9:
+//   case 10:
+//     alert("outstanding");
+//     break;
+//   default:
+//     alert("out of range");
+//     break
+// }
+
+//Exercise 9
+
+// let data = prompt("Please enter DNI or NIE:");
+// //Verify data length, must be 9 char
+// if (data.length == 9){
+// //Verify if data beggins by T or X
+//   if (data.charCodeAt(0) == 84 || data.charCodeAt(0) == 88) {
+//     //Checking if the last char is a letter too
+//     if (
+//       // data.charCodeAt(data.length -1) >= 65 &&
+//       // data.charCodeAt(data.length -1) <= 90 
+//       65 <=
+//       datadata.charCodeAt(data.length - 1) <=
+//       90
+//     ){
+//       //Checking if the other part of the string are numbers
+//       if (
+//       data.charCodeAt(1) >= 48 &&
+//       data.charCodeAt(1) <= 57 &&
+//       data.charCodeAt(2) >= 48 &&
+//       data.charCodeAt(2) <= 57 &&
+//       data.charCodeAt(3) >= 48 &&
+//       data.charCodeAt(3) <= 57 &&
+//       data.charCodeAt(4) >= 48 &&
+//       data.charCodeAt(4) <= 57 &&
+//       data.charCodeAt(5) >= 48 &&
+//       data.charCodeAt(5) <= 57 &&
+//       data.charCodeAt(6) >= 48 &&
+//       data.charCodeAt(6) <= 57 &&
+//       data.charCodeAt(7) >= 48 &&
+//       data.charCodeAt(7) <= 57 &&
+//       data.charCodeAt(8) >= 48 &&
+//       data.charCodeAt(8) <= 57 
+//     ){
+//       console.log("valid NIE");
+//     }else{
+//       console.log("Please, check your NIE");
+//     }
+//     }
+
+// }else{
+//   console.log("Please enter a valid data")
+// }
+
+//Exercise 10
+// Setting max and min possible values
+let max = 6;
+let min = 1;
+// Generating a random number between max and min
+let randomNumber1 = Math.floor(Math.random() * max) + min;
+let randomNumber2 = Math.floor(Math.random() * max) + min;
+console.log(randomNumber1);
+console.log(randomNumber2);
+if(randomNumber1 > randomNumber2){
+  console.log("Player 1 wins!");
+}else if (randomNumber1 < randomNumber2){
+  console.log("Player 2 wins!");
+}else {
+  console.log("Draw");
 }
-
-
-
-
-
