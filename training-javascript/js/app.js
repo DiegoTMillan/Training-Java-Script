f5e2d7ad2738388a7f3376b167ff3314d3ed2f54
Diego <diego_tapia_millan@yahoo.es>
2022-03-22 12:46:43 +0100

    siguiente ej8 me falta el 5

--- training-javascript/js/app.js
+++ training-javascript/js/app.js
@@ -1,106 +1,106 @@
 //Four seasons
 
 //getting real month
-let actualDate = new Date();
-let actualMonth = actualDate.getMonth();
+// let actualDate = new Date();
+// let actualMonth = actualDate.getMonth();
 
-if (actualMonth == 11 || actualMonth == 0 || actualMonth == 1) {
-  season = "Winter";
-} else if (actualMonth == 2 || actualMonth == 3 || actualMonth == 4) {
-  season = "Spring";
-} else if (actualMonth == 5 || actualMonth == 6 || actualMonth == 7) {
-  season = "Summer";
-} else if (actualMonth == 8 || actualMonth == 9 || actualMonth == 10) {
-  season = "Autumn";
-} else {
-  season = "unknown season";
-}
-console.log(season);
-//show month name
-let monthName = "";
-switch (actualMonth) {
-  case 0:
-    monthName = "January";
-    break;
-  case 1:
-    monthName = "February";
-    break;
-  case 2:
-    monthName = "March";
-    break;
-  case 3:
-    monthName = "April";
-    break;
-  case 4:
-    monthName = "May";
-    break;
-  case 5:
-    monthName = "June";
-    break;
-  case 6:
-    monthName = "July";
-    break;
-  case 7:
-    monthName = "August";
-    break;
-  case 8:
-    monthName = "September";
-    break;
-  case 9:
-    monthName = "October";
-    break;
-  case 10:
-    monthName = "November";
-    break;
-  case 11:
-    monthName = "December";
-    break;
-  default:
-    monthName = "unknown";
-}
-console.log(monthName);
+// if (actualMonth == 11 || actualMonth == 0 || actualMonth == 1) {
+//   season = "Winter";
+// } else if (actualMonth == 2 || actualMonth == 3 || actualMonth == 4) {
+//   season = "Spring";
+// } else if (actualMonth == 5 || actualMonth == 6 || actualMonth == 7) {
+//   season = "Summer";
+// } else if (actualMonth == 8 || actualMonth == 9 || actualMonth == 10) {
+//   season = "Autumn";
+// } else {
+//   season = "unknown season";
+// }
+// console.log(season);
+// //show month name
+// let monthName = "";
+// switch (actualMonth) {
+//   case 0:
+//     monthName = "January";
+//     break;
+//   case 1:
+//     monthName = "February";
+//     break;
+//   case 2:
+//     monthName = "March";
+//     break;
+//   case 3:
+//     monthName = "April";
+//     break;
+//   case 4:
+//     monthName = "May";
+//     break;
+//   case 5:
+//     monthName = "June";
+//     break;
+//   case 6:
+//     monthName = "July";
+//     break;
+//   case 7:
+//     monthName = "August";
+//     break;
+//   case 8:
+//     monthName = "September";
+//     break;
+//   case 9:
+//     monthName = "October";
+//     break;
+//   case 10:
+//     monthName = "November";
+//     break;
+//   case 11:
+//     monthName = "December";
+//     break;
+//   default:
+//     monthName = "unknown";
+// }
+// console.log(monthName);
 
-let character = "Doctor Strange";
-let power = "";
-switch (character) {
-  case "Captain America":
-    power = "indestructible shield";
-    break;
-  case "Black Widow":
-    power = "Master spy & assasin";
-    break;
-  case "Iron Man":
-    power = "Genius-level intellect";
-    break;
-  case "Thor":
-    power = "the hammer of Thor";
-    break;
-  case "Doctor Strange":
-    power = "Great Wizard";
-    break;
-  default:
-    console.log("Choose your hero!");
-}
-console.log(power);
+// let character = "Doctor Strange";
+// let power = "";
+// switch (character) {
+//   case "Captain America":
+//     power = "indestructible shield";
+//     break;
+//   case "Black Widow":
+//     power = "Master spy & assasin";
+//     break;
+//   case "Iron Man":
+//     power = "Genius-level intellect";
+//     break;
+//   case "Thor":
+//     power = "the hammer of Thor";
+//     break;
+//   case "Doctor Strange":
+//     power = "Great Wizard";
+//     break;
+//   default:
+//     console.log("Choose your hero!");
+// }
+// console.log(power);
 
-let number = 10;
+// let number = 10;
 
-switch (true) {
-  case number < 0:
-    console.log("Under 0");
-    break;
-  case number >= 0 && number <= 10:
-    console.log("Between 0 and 10");
-    break;
-  case number > 11 && number <= 20:
-    console.log("Between 10 and 20");
-    break;
-  case number > 20:
-    console.log("Over 20");
-    break;
-  default:
-    console.log("out of range");
-}
+// switch (true) {
+//   case number < 0:
+//     console.log("Under 0");
+//     break;
+//   case number >= 0 && number <= 10:
+//     console.log("Between 0 and 10");
+//     break;
+//   case number > 11 && number <= 20:
+//     console.log("Between 10 and 20");
+//     break;
+//   case number > 20:
+//     console.log("Over 20");
+//     break;
+//   default:
+//     console.log("out of range");
+// }
 
 //Exercise 1 
 
@@ -176,24 +176,66 @@ switch (true) {
 //     break;
 // }
 
-//Exercise 5
-let absoluteZero = 273.15;
-let freezingPoint = 0;
-let bodyTemperature = 37;
-let boillingPoint = 100;
-let fahrenheit = (absoluteZero*1.8)+32 || (freezingPoint*1.8)+32 || (bodyTemperature*1.8)+32 || (boillingPoint*1.8)+32;
+//Exercise 5 ESTÁ MAL, NO FUNCIONA
+// let absoluteZero = 273.15;
+// let freezingPoint = 0;
+// let bodyTemperature = 37;
+// let boillingPoint = 100; 
+// let celsius = absoluteZero || freezingPoint || boillingPoint || bodyTemperature;
+// let resultado = celsius * 9/5 + 32;
 
-switch (result){
-  case absoluteZero == (273.15 * 1.8 + 32):
-    console.log(fahrenheit , "ºF");
+// switch (resultado){
+//   case absoluteZero == (273.15 *1.8 + 32):
+//     console.log(fahrenhait);
+//     break;
+//   case freezingPoint = (0 * 1.8 + 32):
+//     console.log(freezingPoint , "ºF");
+//     break;
+//   case bodyTemperature = (37 * 1.8 + 32):
+//     console.log(bodyTemperature , "ºF");
+//     break;
+//   case boillingPoint = (100 * 1.8 + 32):
+//     console.log(boillingPoint , "ºF")
+// }
+
+//Exercise 6
+// let email = "tolkien@lordofrings.com";
+// let password = "aNBR6ZeWPY";
+// let emailCheck = prompt("Please, insert your email");
+// let passwordCheck = prompt ("Please insert your password");
+// if (emailCheck == email && passwordCheck == password) {
+//   alert("Welcome Mr. Tolkien");
+// }else if (emailCheck !== email || passwordCheck !== password){
+//   alert("Wrong password or username");
+// }
+
+//Exercise 7
+let currency = "yen";
+let euro = 200;
+let change = "";
+
+switch (currency) {
+  case "dolar":
+    change = euro * 1.10;
+    console.log(`${euro} euros son ${change} dólares al cambio`);
+    break; 
+  case "yen":
+    change = euro * 132.87;
+    console.log(`${euro} euros son ${change} yenes al cambio`);
     break;
-  case freezingPoint = (0 * 1.8 + 32):
-    console.log(fahrenheit , "ºF");
+  case "libra":
+    change = euro * 0.83;
+    console.log(`${euro} euros son ${change} libras esterlinas al cambio`);
     break;
-  case bodyTemperature = (37 * 1.8 + 32):
-    console.log(fahrenheit , "ºF");
+  case "franco":
+    change = euro * 1.03;
+    console.log(`${euro} euros son ${change} francos suizos al cambio`);
     break;
-  case boillingPoint = (100 * 1.8 + 32):
-    console.log(fahrenheit , "ºF")
+  default:
+    console.log(unknown);   
 }
+
+
+
+
 
