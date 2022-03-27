9e1cdcafc984931d2d002a076d0112e7d4d5fed9
Diego <diego_tapia_millan@yahoo.es>
2022-03-27 18:27:47 +0200

    dibujo casi hecho, terminar ej 6 por mi mismo

--- training-javascript/js/exercises.js
+++ training-javascript/js/exercises.js
@@ -376,7 +376,6 @@
 // }
 // console.log(invertPal);
 
-
 //Exercise 3
 
 // let repeat = 4 ;
@@ -384,6 +383,38 @@
 //     console.log("****");
 // }
 //Busqué la opción en la consola de desagrupar los elementos repetidos
+
+//Jorge answer
+
+// let msg = "";
+// let side = 10;
+// for (let i = 0; i < side; i++) {
+//   for (let j = 0; j < side; j++) {
+//     msg = msg + "* ";
+//   }
+//   msg = msg + "\n";
+// }
+// console.log(msg);
+
+// let msg = "";
+// let size = prompt("Enter the size of the pyramid");
+// for(let i=1; i<=size; i++)
+// {
+//   msg = msg + "* "
+//   console.log(msg);
+// }
+let base = 10
+for (let i = 0; i < base; i++) {
+    let picture = "";
+    for (let j =0; j < base - i; j++){
+      picture += " ";  
+    } 
+    for (let k = 0; k <= i; k++){
+      picture += "* ";  
+    } 
+    console.log(picture);  
+} 
+
 
 //Teacher solution
 
@@ -468,23 +499,120 @@
 
 //Exercise 6
 
-// let string= "";
-// let numPass = prompt("Please, write the number of characters between 8 and 16 for your password")
+// let password= "";
+// let numPass = parseInt(prompt("Please, write the number of characters between 8 and 16 for your password"));
 // let lastPass = 16;
 // let optMinus = prompt("Do you want lowercase characters? please write Yes or Not").toLowerCase();
 // let optMayus = prompt("Do you want capital letters? Please, write Yes or Not").toLowerCase();
 // let optNum = prompt("Do you want numbers? Please, write Yes or Not").toLowerCase();
 // let optSim = prompt ("Do you want symbols? Please, write Yes or Not").toLowerCase();
-// for(let i = 8; numPass < lastPass; numPass++){
-//     if (numPass = i && optMinus == "yes" || "y" && optMayus == "yes" || "y" && optNum == "yes" || "y" || optSim == "yes" || "y"){
-//         for(j=0; j < i; j++){
-//             if(string.fromCharCode(Math.floor(Math.random() * (170 - 33 + 1) + 33))){
-//                 alert(string);
-//             }
+// for(numPass; numPass < lastPass; numPass++){
+//     if (optMinus == "yes" && optMayus == "yes" && optNum == "yes" && optSim == "yes"){
+//         for(j=0; j < numPass; j++){
+//             password += String.fromCharCode((Math.floor(Math.random() * (122 - 33 + 1) + 33)))
 //         }
+//         alert(`Your password is: ${password}`)
+//         break;
+//     }
+//     if (optMinus == "yes" && optMayus == "yes" && optNum == "yes" && optSim == "not"){
+//         for(j=0; j < numPass; j++){
+//             password += String.fromCharCode((Math.floor(Math.random() * (57 - 48 + 1) + 48))) && String.fromCharCode((Math.floor(Math.random() * (90 - 65 + 1) + 65))) && String.fromCharCode((Math.floor(Math.random() * (122 - 97 + 1) + 97)))
+//         }
+//         alert(`Your password is: ${password}`)
+//         break;
 //     }
 // }
 
+// Exercise 6
 
+// let length = parseInt(prompt("Please enter the password length (8 - 16)"));
+// length = length >= 8 && length <= 16 ? length : 8;
+// console.log(length);
 
+// let includeLower = prompt(
+//   "Would you like to include lowers (y for yes, n for no)"
+// ).toLowerCase();
+// let withLowers = includeLower == "n" ? false : true;
 
+// let includeUpper = prompt(
+//   "Would you like to include uppers (y for yes, n for no)"
+// ).toLowerCase();
+// let withUppers = includeUpper == "n" ? false : true;
+
+// let includeNumber = prompt(
+//   "Would you like to include numbers (y for yes, n for no)"
+// ).toLowerCase();
+// let withNumbers = includeNumber == "n" ? false : true;
+
+// let includeSymbol = prompt(
+//   "Would you like to include symbols (y for yes, n for no)"
+// ).toLowerCase();
+
+// let withSymbols = includeSymbol == "n" ? false : true;
+
+// !withLowers &&
+//   !withUppers &&
+//   !withNumbers &&
+//   !withSymbols &&
+//   (withLowers = !withLowers);
+// let password = "";
+// let repeatedChar = false;
+// let countingGenPass = 0;
+// while (
+//   (withLowers && !/[a-z]/.test(password)) ||
+//   (withUppers && !/[A-Z]/.test(password)) ||
+//   (withNumbers && !/[0-9]/.test(password)) ||
+//   (withSymbols &&
+//     !/(!|"|#|[$]|%|&|'|[(]|[)]|[*]|[+]|'|-|.|[/])+/.test(password)) ||
+//   repeatedChar
+// ) {
+//   countingGenPass++;
+//   password = "";
+//   repeatedChar = false;
+//   for (let i = 0, newChar = false; i < length; i++) {
+//     while (!newChar) {
+//       switch (Math.floor(Math.random() * 4) + 1) {
+//         case 1:
+//           if (withLowers) {
+//             password += String.fromCharCode(
+//               Math.floor(Math.random() * (122 - 97 + 1)) + 97
+//             );
+//             newChar = !newChar;
+//           }
+//           break;
+//         case 2:
+//           if (withUppers) {
+//             password += String.fromCharCode(
+//               Math.floor(Math.random() * (90 - 65 + 1)) + 65
+//             );
+//             newChar = !newChar;
+//           }
+//           break;
+//         case 3:
+//           if (withNumbers) {
+//             password += String.fromCharCode(
+//               Math.floor(Math.random() * (57 - 48 + 1)) + 48
+//             );
+//             newChar = !newChar;
+//           }
+//           break;
+//         case 4:
+//           if (withSymbols) {
+//             password += String.fromCharCode(
+//               Math.floor(Math.random() * (47 - 33 + 1)) + 33
+//             );
+//             newChar = !newChar;
+//           }
+//           break;
+//       }
+//     }
+//     newChar = !newChar;
+//   }
+//   for (let i = 0; i < password.length - 1; i++) {
+//     if (password[i] == password[i + 1]) {
+//       repeatedChar = true;
+//       break;
+//     }
+//   }
+// }
+// alert(`Your password: ${password} in ${countingGenPass} attemps.`);
