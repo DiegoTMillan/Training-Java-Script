04718041d9fe7842b881c1fc82db2b08e2effe5d
Diego <diego_tapia_millan@yahoo.es>
2022-03-31 16:17:26 +0200

    finished third exercise of algorithms3

--- training-javascript/js/algorithms3.js
+++ training-javascript/js/algorithms3.js
@@ -68,7 +68,16 @@
 //     ("There is a space-time anomaly")
 // }
 
-let hexColor = "";
+let hexColor1 = "";
+let hexColor2 = "";
+let hexColor3 = "";
+let hexColor4 = "";
+let hexColor5 = "";
+let hexColor6 = "";
+let hexColor7 = "";
+let hexColor8 = "";
+let hexColor9 = "";
+let hexColor10 = "";
 let hCLength = 6;
 
 const randomNumber = function (min, max) {
@@ -80,13 +89,75 @@ const randomNumber = function (min, max) {
 for (let i = 0; i < hCLength; i++) {
     switch (randomNumber(1, 2)) {
       case 1:
-        hexColor += String.fromCharCode(randomNumber(48, 57));
+        hexColor1 += String.fromCharCode(randomNumber(48, 57));
+        hexColor2 += String.fromCharCode(randomNumber(48, 57));
+        hexColor3 += String.fromCharCode(randomNumber(48, 57));
+        hexColor4 += String.fromCharCode(randomNumber(48, 57));
+        hexColor5 += String.fromCharCode(randomNumber(48, 57));
+        hexColor6 += String.fromCharCode(randomNumber(48, 57));
+        hexColor7 += String.fromCharCode(randomNumber(48, 57));
+        hexColor8 += String.fromCharCode(randomNumber(48, 57));
+        hexColor9 += String.fromCharCode(randomNumber(48, 57));
+        hexColor10 += String.fromCharCode(randomNumber(48, 57));
         break;
       case 2:
-        hexColor += String.fromCharCode(randomNumber(97, 102));
+        hexColor1 += String.fromCharCode(randomNumber(97, 102));
+        hexColor2 += String.fromCharCode(randomNumber(97, 102));
+        hexColor3 += String.fromCharCode(randomNumber(97, 102));
+        hexColor4 += String.fromCharCode(randomNumber(97, 102));
+        hexColor5 += String.fromCharCode(randomNumber(97, 102));
+        hexColor6 += String.fromCharCode(randomNumber(97, 102));
+        hexColor7 += String.fromCharCode(randomNumber(97, 102));
+        hexColor8 += String.fromCharCode(randomNumber(97, 102));
+        hexColor9 += String.fromCharCode(randomNumber(97, 102));
+        hexColor10 += String.fromCharCode(randomNumber(97, 102));
         break;  
-    }for ( j= 0; j < 10; j++){
-console.log(`#${hexColor}`)
-}
+    }
   }
+// }
+// for ( j= 0; j < 10; j++){
+console.log(`#${hexColor1}`)
+console.log(`#${hexColor2}`)
+console.log(`#${hexColor3}`)
+console.log(`#${hexColor4}`)
+console.log(`#${hexColor5}`)
+console.log(`#${hexColor6}`)
+console.log(`#${hexColor7}`)
+console.log(`#${hexColor8}`)
+console.log(`#${hexColor9}`)
+console.log(`#${hexColor10}`)
+
+switch(randomNumber(1, 10)){
+    case 1:
+        console.log("%cHello World", "color:" + "#" + hexColor1);
+        break;
+        case 2:
+        console.log("%cHello World", "color:" + "#" + hexColor2);
+        break;
+        case 3:
+        console.log("%cHello World", "color:" + "#" + hexColor3);
+        break;
+        case 4:
+        console.log("%cHello World", "color:" + "#" + hexColor4);
+        break;
+        case 5:
+        console.log("%cHello World", "color:" + "#" + hexColor5);
+        break;
+        case 6:
+        console.log("%cHello World", "color:" + "#" + hexColor6);
+        break;
+        case 7:
+        console.log("%cHello World", "color:" + "#" + hexColor7);
+        break;
+        case 8:
+        console.log("%cHello World", "color:" + "#" + hexColor8);
+        break;
+        case 9:
+        console.log("%cHello World", "color:" + "#" + hexColor9);
+        break
+        case 10:
+        console.log("%cHello World", "color:" + "#" + hexColor10);
+        break
+}
+// break;
 // }
