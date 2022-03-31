0f3a29a568de098749140a69a9d6022c647d981c
Diego <diego_tapia_millan@yahoo.es>
2022-03-31 19:02:16 +0200

    in fourth exercise

--- training-javascript/js/algorithms3.js
+++ training-javascript/js/algorithms3.js
@@ -2,7 +2,19 @@
 //EXERCISES MOD 3
 //----------
 
+//UTILITIES
+
+const randomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const referenceHour = function (h, min) {
+  return h * 60 + min;
+};
+
+//----------
 //Exercise 3.1.
+//----------
 
 const exercise1 = () => {
   //User properties
@@ -10,13 +22,8 @@ const exercise1 = () => {
   let userMaxLimit = parseInt(prompt("Please, enter max limit"));
   let userMinLimit = parseInt(prompt("Please, enter min limit"));
 
-  //Getting a Random Number with this Function
-
-  const randomNumber = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
   //Preparing array
+
   let numberOfElements = 20;
   let resultingArray = new Array(numberOfElements);
   // console.log(resultingArray)
@@ -52,8 +59,11 @@ const exercise1 = () => {
 
 // exercise1();
 
+//------------
+//Exercise 3.2
+//------------
+
 const exercise2 = () => {
-  //Exercise 3.2
 
   //Getting Hour and minutes from system
 
@@ -68,11 +78,8 @@ const exercise2 = () => {
   //let HoursToMinutes = 0
   //console.log(HoursToMinutes);
 
-  //Preparing Function for decimal number from hours and minutes reference
+  //Using function to convert hours to mins
 
-  const referenceHour = function (h, min) {
-    return h * 60 + min;
-  };
   let firstHour = referenceHour(6, 0);
   let secondHour = referenceHour(11, 59);
   let thirdHour = referenceHour(12, 0);
@@ -97,29 +104,28 @@ const exercise2 = () => {
 
 // exercise2();
 
+//-----------
 //Exercise 3.3
+//-----------
 
 const exercise3 = () => {
+  //Preparing the strings
 
-let hexColor1 = "";
-let hexColor2 = "";
-let hexColor3 = "";
-let hexColor4 = "";
-let hexColor5 = "";
-let hexColor6 = "";
-let hexColor7 = "";
-let hexColor8 = "";
-let hexColor9 = "";
-let hexColor10 = "";
-let hCLength = 6;
+  let hexColor1 = "";
+  let hexColor2 = "";
+  let hexColor3 = "";
+  let hexColor4 = "";
+  let hexColor5 = "";
+  let hexColor6 = "";
+  let hexColor7 = "";
+  let hexColor8 = "";
+  let hexColor9 = "";
+  let hexColor10 = "";
+  let hCLength = 6;
 
-const randomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+  //Creating de different colors
 
-// console.log(letterOrNumber)
-// for ( j= 0; j < 10; j++){
-for (let i = 0; i < hCLength; i++) {
+  for (let i = 0; i < hCLength; i++) {
     switch (randomNumber(1, 2)) {
       case 1:
         hexColor1 += String.fromCharCode(randomNumber(48, 57));
@@ -147,51 +153,81 @@ for (let i = 0; i < hCLength; i++) {
         break;
     }
   }
-// }
-// for ( j= 0; j < 10; j++){
-console.log(`#${hexColor1}`)
-console.log(`#${hexColor2}`)
-console.log(`#${hexColor3}`)
-console.log(`#${hexColor4}`)
-console.log(`#${hexColor5}`)
-console.log(`#${hexColor6}`)
-console.log(`#${hexColor7}`)
-console.log(`#${hexColor8}`)
-console.log(`#${hexColor9}`)
-console.log(`#${hexColor10}`)
 
-switch(randomNumber(1, 10)){
+  //Showing hexadecimal colors in console
+
+  console.log(`#${hexColor1}`);
+  console.log(`#${hexColor2}`);
+  console.log(`#${hexColor3}`);
+  console.log(`#${hexColor4}`);
+  console.log(`#${hexColor5}`);
+  console.log(`#${hexColor6}`);
+  console.log(`#${hexColor7}`);
+  console.log(`#${hexColor8}`);
+  console.log(`#${hexColor9}`);
+  console.log(`#${hexColor10}`);
+
+  //showing "Hello World" in different colors
+
+  switch (randomNumber(1, 10)) {
     case 1:
-        console.log("%cHello World", "color:" + "#" + hexColor1);
-        break;
-        case 2:
-        console.log("%cHello World", "color:" + "#" + hexColor2);
-        break;
-        case 3:
-        console.log("%cHello World", "color:" + "#" + hexColor3);
-        break;
-        case 4:
-        console.log("%cHello World", "color:" + "#" + hexColor4);
-        break;
-        case 5:
-        console.log("%cHello World", "color:" + "#" + hexColor5);
-        break;
-        case 6:
-        console.log("%cHello World", "color:" + "#" + hexColor6);
-        break;
-        case 7:
-        console.log("%cHello World", "color:" + "#" + hexColor7);
-        break;
-        case 8:
-        console.log("%cHello World", "color:" + "#" + hexColor8);
-        break;
-        case 9:
-        console.log("%cHello World", "color:" + "#" + hexColor9);
-        break
-        case 10:
-        console.log("%cHello World", "color:" + "#" + hexColor10);
-        break
-}
-}
+      console.log("%cHello World", "color:" + "#" + hexColor1);
+      break;
+    case 2:
+      console.log("%cHello World", "color:" + "#" + hexColor2);
+      break;
+    case 3:
+      console.log("%cHello World", "color:" + "#" + hexColor3);
+      break;
+    case 4:
+      console.log("%cHello World", "color:" + "#" + hexColor4);
+      break;
+    case 5:
+      console.log("%cHello World", "color:" + "#" + hexColor5);
+      break;
+    case 6:
+      console.log("%cHello World", "color:" + "#" + hexColor6);
+      break;
+    case 7:
+      console.log("%cHello World", "color:" + "#" + hexColor7);
+      break;
+    case 8:
+      console.log("%cHello World", "color:" + "#" + hexColor8);
+      break;
+    case 9:
+      console.log("%cHello World", "color:" + "#" + hexColor9);
+      break;
+    case 10:
+      console.log("%cHello World", "color:" + "#" + hexColor10);
+      break;
+  }
+};
 
-exercise3()
+// exercise3()
+
+//----------
+//Exercise 4
+//----------
+
+// let userDni = prompt("Please write a DNI with this format 00000000-A")
+let userDni = "40694707-W"
+
+let carRegistrationCodeArray = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+// console.log(carRegistrationCodeArray)
+
+let dniObject = userDni.split("-");
+let dniNumbersArray = Object.values(dniObject[0]);
+let lastLetter = Object.values(dniObject[1])
+console.log(lastLetter)
+let dniNumbers = parseInt(dniNumbersArray);
+console.log(dniNumbers)
+
+// let sumOfNumbers = parseInt(dniNumbersArray);
+// let sum = 0;
+// for (let num of sumOfNumbers){
+//   sum += num;
+// }
+// console.log(sum);
+
+
+
