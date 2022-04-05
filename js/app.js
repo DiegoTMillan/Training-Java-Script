9a95d74144830af54a71f2520cdc5eac935bd45b
Diego <diego_tapia_millan@yahoo.es>
2022-04-05 20:35:39 +0200

    finishing examples with remove and replace in DOM with js

--- js/app.js
+++ js/app.js
@@ -22,12 +22,55 @@
 // console.log(headersAndParagraphs[0].firstChild.nodeValue);
 // console.log(nav.nodeName);
 
-let box1 = document.querySelector("section").firstElementChild;
-let box2 = box1.nextElementSibling;
-let box3 = box2.nextElementSibling;
+// let box1 = document.body.children[0].firstElementChild;
+// let box2 = box1.nextElementSibling;
+// let box3 = box2.nextElementSibling;
 
-box1.classList.add("dark");
+// box1.classList.add("dark");
 // box1.classList.remove("dark")
-box2.classList.add("dissapear");
+// box2.classList.add("light");
+// box2.classList.remove("light")
+// box2.classList.add("dissapear")
 // box2.classList.remove("dissapear");
+
+// box2.classList.toggle("light");
+// // box2.classList.toggle("light");
+// // box2.classList.toggle("light");
+// //toggle lo enciende y apaga sucesivamente
+
+// console.log(box3.classList.contains("light")) //false, porque no contiene esa clase
+// console.log(box2.classList.contains("light"));//True porque el toggle solo está una vez y está activo en el box 2 la clase light
+
+// let number = document.querySelector("section").lastElementChild.previousElementSibling.dataset.boxNumber;
+// console.log(number);//devuelve los datos o referencias aportados en el html
+
+// document.body.innerHTML = "<h1>Waw</h1>"
+// console.log(document.querySelector("section").innerHTML)
+// console.log(document.querySelector("section").outerHTML)
+// document.querySelector("section").outerHTML = "<h1>Waw</h1>"
+
+// document.body.insertAdjacentHTML('afterbegin','<nav class="nav">Navigation </nav>');
+// document.querySelector("nav").insertAdjacentHTML("beforebegin", '<h1>Main Header</h1>')
+// document.querySelector("nav").insertAdjacentHTML("beforeend", '<a href="https://google.com">Go google</a>')
+// document.querySelector("nav").insertAdjacentHTML("afterend", "<p>Lorem ipsum dolor sit amet.</p>")
+// console.log(document.querySelector("h1").textContent);
+// document.querySelector("h1").textContent = "Stronger together"
+
+let header = document.createElement("h1");
+header.append("Hello World!");
+header.prepend("¡");
+document.body.prepend(header);
+document.body.append(header);
+document.querySelector("h1").firstChild.before("I'm happy ")
+let paragraph = document.createElement("p");
+paragraph.textContent = "Writting a paragraph";
+// document.body.append(paragraph)
+document.querySelector("h1").after(paragraph);
+document.querySelector("h1").replaceWith(paragraph)//sustituido h1 por párrafo
+paragraph.remove();
+
+
+
+
+
 
